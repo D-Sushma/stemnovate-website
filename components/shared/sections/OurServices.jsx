a7b1d4f66513b88c1a08eab5843708ed8d18af04
Sar-Software-Solutions <info@sarmicrosystems.in>
2022-12-15 17:25:23 +0530

    Modified the services of home page

--- components/shared/sections/OurServices.jsx
+++ components/shared/sections/OurServices.jsx
@@ -16,59 +16,72 @@ const OurService = ({ title = "OUR SERVICES" }) => {
           <div className="row">
             <div className="col-md-6 text-center">
               <div>
-                <a href="/Applications/Drug-Discovery-Platform">
-                  <div className="overflow-hidden mx-4">
-                    <Image
-                      className="ps-banner__image"
-                      src={Discovery}
-                      alt="Drug Discovery Platform"
-                      width="500px"
-                      height="281px"
-                    />
-                  </div>
-
-                  <h3 className="text-white m-4">Drug Discovery Platform</h3>
-                  <p className="text-left  text-white">
-                    We use novel multi-organ modelling to evaluate drug molecule
-                    metabolism and toxicity, ensuring better clinical trials and
-                    patient safety outcomes. Our projects include Hepatitis
-                    research and many more.
-                  </p>
-                </a>
-                <div className="button button--yellow">
+                <div className="row">
                   <a href="/Applications/Drug-Discovery-Platform">
-                    READ MORE <FaArrowRight />
+                    <div className="overflow-hidden mx-4">
+                      <Image
+                        className="ps-banner__image"
+                        src={Discovery}
+                        alt="Drug Discovery Platform"
+                        width="500px"
+                        height="281px"
+                      />
+                    </div>
+
+                    <h3 className="text-white m-4">Drug Discovery Platform</h3>
+                    <p className="text-left  text-white">
+                      We use novel multi-organ modelling to evaluate drug
+                      molecule metabolism and toxicity, ensuring better clinical
+                      trials and patient safety outcomes. Our projects include
+                      Hepatitis research and many more.
+                    </p>
                   </a>
+                </div>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <div className="button button--yellow">
+                      <a href="/Applications/Drug-Discovery-Platform">
+                        READ MORE <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="col-md-6 text-center">
               <div>
-                <a href="/Products">
-                  <div className="overflow-hidden mx-4">
-                    <Image
-                      className="ps-banner__image"
-                      src={bioImg}
-                      alt="Bio Banking"
-                      width={"500px"}
-                      height="281px"
-                    />
-                  </div>
-                  <h3 className="text-white m-4">Bio Banking</h3>
-                  <p className="mx-4 text-left text-white">
-                    Ethical sourcing is core to our biobank, reflecting human
-                    diversity in age, gender & ethnicities. In addition, we
-                    offer a multispecies cell platform ensuring quality through
-                    advanced profiling & bioinformatics.
-                  </p>
-                </a>
+                <div className="row">
+                  <a href="/Products">
+                    <div className="overflow-hidden mx-4">
+                      <Image
+                        className="ps-banner__image"
+                        src={bioImg}
+                        alt="Bio Banking"
+                        width={"500px"}
+                        height="281px"
+                      />
+                    </div>
+                    <h3 className="text-white m-4">Bio Banking</h3>
+                    <p className="mx-4 text-left text-white">
+                      Ethical sourcing is core to our biobank, reflecting human
+                      diversity in age, gender & ethnicities. In addition, we
+                      offer a multispecies cell platform ensuring quality
+                      through advanced profiling & bioinformatics.
+                    </p>
+                  </a>
+                </div>
+
                 {/* <a className=" text-uppercase btn btn-lg button-orange text-white m-4" href="/Products">
                                     View Products
                                 </a> */}
-                <div className="button button--yellow">
-                  <a href="/Products">
-                    VIEW PRODUCTS <FaArrowRight />
-                  </a>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <div className="button button--yellow">
+                      <a href="/Products">
+                        VIEW PRODUCTS <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -80,61 +93,73 @@ const OurService = ({ title = "OUR SERVICES" }) => {
           <div className="row">
             <div className="col-md-6 text-center">
               <div>
-                <a href="/Services">
-                  <div className="overflow-hidden mx-4">
-                    <Image
-                      className="ps-banner__image"
-                      src={Reprogramming}
-                      alt="Reprogramming and Differentiation"
-                      width={"500px"}
-                      height="281px"
-                    />
-                  </div>
-                  <h3 className="text-black m-4">
-                    Reprogramming and Differentiation
-                  </h3>
-                  <p className="mx-4 text-left text-black">
-                    We are experts in cellular reprogramming and one of the only
-                    companies globally with the capability for human and animal
-                    reprogramming. We design vectors with an industrial process
-                    to generate induced pluripotent stem cells. For industry and
-                    academia, we routinely differentiate stem cells into the
-                    liver, heart, and neuronal cells.
-                  </p>
-                </a>
-                <div className="button button--yellow">
+                <div className="row">
                   <a href="/Services">
-                    READ MORE <FaArrowRight />
+                    <div className="overflow-hidden mx-4">
+                      <Image
+                        className="ps-banner__image"
+                        src={Reprogramming}
+                        alt="Reprogramming and Differentiation"
+                        width={"500px"}
+                        height="281px"
+                      />
+                    </div>
+                    <h3 className="text-black m-4">
+                      Reprogramming and Differentiation
+                    </h3>
+                    <p className="mx-4 text-left text-black">
+                      We are experts in cellular reprogramming and one of the
+                      only companies globally with the capability for human and
+                      animal reprogramming. We design vectors with an industrial
+                      process to generate induced pluripotent stem cells. For
+                      industry and academia, we routinely differentiate stem
+                      cells into the liver, heart, and neuronal cells.
+                    </p>
                   </a>
+                </div>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <div className="button button--yellow">
+                      <a href="/Services">
+                        READ MORE <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="col-md-6 text-center">
               <div>
-                <a href="/Products">
-                  <div className="overflow-hidden mx-4">
-                    <Image
-                      className="ps-banner__image"
-                      src={chipTechnology}
-                      alt="Chip Technology"
-                      width={"500px"}
-                      height="281px"
-                    />
-                  </div>
-                  <h3 className="text-black m-4">Chip Technology</h3>
-                  <p className="mx-4 text-left text-black">
-                    We are innovating organs-on-chip platforms to provide a
-                    physiological alternative to cancer lines and animal models
-                    for advanced drug discovery. From 2017 to 2019, our research
-                    was co-funded by Innovate UK to develop a liver on chip
-                    platform. This million-pound funding resulted in patenting
-                    innovation that we aim to launch to market soon.
-                  </p>
-                </a>
-                <div className="button button--yellow">
+                <div className="row">
                   <a href="/Products">
-                    VIEW PRODUCTS <FaArrowRight />
+                    <div className="overflow-hidden mx-4">
+                      <Image
+                        className="ps-banner__image"
+                        src={chipTechnology}
+                        alt="Chip Technology"
+                        width={"500px"}
+                        height="281px"
+                      />
+                    </div>
+                    <h3 className="text-black m-4">Chip Technology</h3>
+                    <p className="mx-4 text-left text-black">
+                      We are innovating organs-on-chip platforms to provide a
+                      physiological alternative to cancer lines and animal
+                      models for advanced drug discovery. From 2017 to 2019, our
+                      research was co-funded by Innovate UK to develop a liver
+                      on chip platform. This million-pound funding resulted in
+                      patenting innovation that we aim to launch to market soon.
+                    </p>
                   </a>
+                </div>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <div className="button button--yellow">
+                      <a href="/Products">
+                        VIEW PRODUCTS <FaArrowRight />
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
