1ce6b50f8a55c986b07a2478e82838cd01ccca8d
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2023-10-16 17:40:49 +0530

    preload background image

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,6 +17,8 @@ class MyDocument extends Document {
                     <meta name="theme-color" content="#fff" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link rel="preload" as="image" href="/static/home-images/Bg01.jpg" />
+                    <link rel="preload" as="image" href="/static/home-images/bg-02.jpg" />
                     {/* <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link> */}
                     <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10846076758" strategy="afterInteractive" />
                     <Script async id="google-analytics" strategy="afterInteractive">
